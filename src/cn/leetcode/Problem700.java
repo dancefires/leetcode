e126e54a6f883b405fdb5023a6961aa3b940a002
cm <3er4#ER$>
2020-06-18 16:39:52 +0800

    提交Problem700代码

--- src/cn/leetcode/Problem700.java
+++ src/cn/leetcode/Problem700.java
@@ -1,38 +1,36 @@
 package cn.leetcode;
 
 /**
- * 
-  *给定二叉搜索树（BST）的根节点和一个值。 
-  *你需要在BST中找到节点值等于给定值的节点。 返回以该节点为根的子树。 
-  *如果节点不存在，则返回 NULL。
-  *例如，
-  *给定二叉搜索树:
- *        4
- *       / \
- *      2   7
- *     / \
- *    1   3
-  *和值: 2
- *
-  *你应该返回如下子树:
- *      2     
- *     / \   
- *    1   3
-  *在上述示例中，如果要找的值是 5，但因为没有节点值为 5，我们应该返回 NULL。
+ * 给定二叉搜索树（BST）的根节点和一个值。
+ * 你需要在BST中找到节点值等于给定值的节点。 返回以该节点为根的子树。
+ * 如果节点不存在，则返回 NULL。
+ * 例如，
+ * 给定二叉搜索树:
+ * 4
+ * / \
+ * 2   7
+ * / \
+ * 1   3
+ * 和值: 2
+ * 你应该返回如下子树:
+ * 2
+ * / \
+ * 1   3
+ * 在上述示例中，如果要找的值是 5，但因为没有节点值为 5，我们应该返回 NULL。
  */
 public class Problem700 {
 
     public static void main(String[] args) {
         Problem700 p = new Problem700();
-        TreeNode root = p.new TreeNode(4);
-        root.left = p.new TreeNode(2);
-        root.right = p.new TreeNode(7);
-        root.left.left = p.new TreeNode(1);
-        root.left.right = p.new TreeNode(3);
+        TreeNode root = new TreeNode(4);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(7);
+        root.left.left = new TreeNode(1);
+        root.left.right = new TreeNode(3);
         System.out.println(p.searchBST(root, 2).val);
     }
 
-    class TreeNode {
+    static class TreeNode {
         int val;
         TreeNode left;
         TreeNode right;
@@ -43,9 +41,9 @@ public class Problem700 {
     }
 
     public TreeNode searchBST(TreeNode root, int val) {
-        if (root == null || val == root.val)
+        if (root == null || val == root.val) {
             return root;
-
+        }
         return val < root.val ? searchBST(root.left, val) : searchBST(root.right, val);
     }
 

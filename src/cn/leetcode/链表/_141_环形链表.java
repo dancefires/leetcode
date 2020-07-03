817d03c1fcabc1ab4b14ea7e1584a5af3e10576a
cm <3er4#ER$>
2020-07-03 11:57:49 +0800

    提交第141题

---
+++ src/cn/leetcode/链表/_141_环形链表.java
@@ -0,0 +1,31 @@
+package cn.leetcode.链表;
+
+public class _141_环形链表 {
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+        ListNode slow = head.next;
+        ListNode fast = head.next.next;
+        while (fast != null && fast.next != null) {
+            if (fast == slow) {
+                return true;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return false;
+    }
+
+}

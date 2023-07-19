9370f14af95373b1d38ac4eacc5eca7176b97fda
everget <everget@dingtalk.com>
2023-07-19 15:02:46 +0800

    traceNo：添加704题解

---
+++ src/cn/leetcode/数组/_704_二分查找.java
@@ -0,0 +1,30 @@
+package cn.leetcode.数组;
+
+/**
+ * @author : cm
+ * @version : v1.0
+ * @desc : https://leetcode.cn/problems/binary-search/
+ * @createTime : 2023-07-19 10:46:24
+ */
+public class _704_二分查找 {
+
+    public int search(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+
+        while (low <= high) {
+            int mid = (low + high) >>> 1;
+            long midVal = nums[mid];
+
+            if (midVal < target) {
+                low = mid + 1;
+            } else if (midVal > target) {
+                high = mid - 1;
+            } else {
+                return mid; // target found
+            }
+        }
+        return -1;  // target not found.
+    }
+
+}
